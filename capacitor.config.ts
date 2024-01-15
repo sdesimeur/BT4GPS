03d56ba025691b427ef7e1ad2b0540a2c95cae4f
sdesimeur <pro@sdesimeur.com>
2024-01-15 23:05:55 +0100

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BT4GPS',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
