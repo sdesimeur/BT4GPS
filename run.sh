888c1f14687e311fb0fa578ef994a9b044b9bcea
sdesimeur <pro@sdesimeur.com>
2024-01-22 23:30:02 +0100

    add function for bluetooth LN Feature

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ionic capacitor run android -l --external --host=127.0.0.1 --port=8100 --watch
 adb reverse tcp:8100 tcp:8100
+ionic capacitor run android -l --external --host=127.0.0.1 --port=8100 --watch
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Inpu
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { Geolocation, PositionOptions, WatchPositionCallback, Position } from '@capacitor/geolocation';
-
+import * as blePeripheral from 'cordova-plugin-ble-peripheral/www/blePeripheral.js';
 
 const watchPositionOptions: PositionOptions = {
   enableHighAccuracy: true,
@@ -10,6 +10,44 @@ const watchPositionOptions: PositionOptions = {
   maximumAge: 0,
 }
 
+const SERVICE_UUID: string = "00001819-0000-1000-8000-00805f9b34fb";
+const CHARACTERISTIC_UUID_LN_FEATURE: string = "00002a6a-0000-1000-8000-00805f9b34fb";
+const CHARACTERISTIC_UUID_LocationAndSpeedCharacteristic: string = "00002a67-0000-1000-8000-00805f9b34fb";
+const CHARACTERISTIC_UUID_NAVIGATION: string = "00002a68-0000-1000-8000-00805f9b34fb";
+
+
+const locationAndNAvigationService = {
+  uuid: SERVICE_UUID,
+  characteristics: [
+      {
+          uuid: CHARACTERISTIC_UUID_LN_FEATURE,
+          properties: blePeripheral.property.READ,
+          permissions: blePeripheral.permission.READABLE | blePeripheral.property.NOTIFY
+      },
+      {
+          uuid: CHARACTERISTIC_UUID_LocationAndSpeedCharacteristic,
+          properties: blePeripheral.property.READ | blePeripheral.property.NOTIFY,
+          permissions: blePeripheral.permission.READABLE,
+          descriptors: [
+              {
+                  uuid: '2901',
+                  value: 'Locationm and Speed Characteristic'
+              }
+          ]
+      },
+      {
+        uuid: CHARACTERISTIC_UUID_NAVIGATION,
+        properties: blePeripheral.property.READ | blePeripheral.property.NOTIFY,
+        permissions: blePeripheral.permission.READABLE,
+        descriptors: [
+            {
+                uuid: '2901',
+                value: 'Navigation'
+            }
+        ]
+    },
+  ]
+};
 
 @Component({
   selector: 'app-home',
@@ -22,8 +60,8 @@ const watchPositionOptions: PositionOptions = {
 
 export class HomePage {
   constructor() {
-
-    
+    blePeripheral.createServiceFromJSON(locationAndNAvigationService);
+    blePeripheral.startAdvertising(locationAndNAvigationService.uuid, 'LN Feature');
   }
 
   _checked : boolean = false;
@@ -42,6 +80,8 @@ export class HomePage {
   watchPositionId: string|undefined = undefined;
 
   parseNewLocation: WatchPositionCallback = (location: Position | null, err: any) => {
+    let ret = new Uint8Array(8);
+    blePeripheral.setCharacteristicValue(SERVICE_UUID, CHARACTERISTIC_UUID_LocationAndSpeedCharacteristic, bytes);
     console.log(location);
   }
 
@@ -54,5 +94,19 @@ export class HomePage {
   async activateLocation () {
     if (this.watchPositionId !== undefined) return;
     this.watchPositionId = await Geolocation.watchPosition(watchPositionOptions, this.parseNewLocation.bind(this) as WatchPositionCallback);
+  }
+
+
+  bytesToString(buffer: ArrayBuffer): string {
+    return String.fromCharCode.apply(null, new Uint8Array(buffer));
+  }
+
+// only works for ASCII characters
+  stringToBytes(string: string): ArrayBuffer {
+    var array = new Uint8Array(string.length);
+    for (var i = 0, l = string.length; i < l; i++) {
+        array[i] = string.charCodeAt(i);
+    }
+    return array.buffer;
   }
 }
